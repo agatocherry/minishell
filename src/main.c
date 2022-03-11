ea0a4bf4f36dd9d5f24254c3eb81f26fcf98c63c
Shellen DORLIN <shdorlin@e2r10p5.clusters.42paris.fr>
2022-03-11 11:17:49 +0100

    main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: agcolas <agcolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 10:47:49 by agcolas           #+#    #+#             */
-/*   Updated: 2022/03/11 10:59:24 by agcolas          ###   ########.fr       */
+/*   Updated: 2022/03/11 11:17:35 by shdorlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,15 @@ int	main(int argc, char **argv, char **env)
 	t_shell shell;
 
 	shell = (t_shell){0};
+	shell.env = get_env(env);
+	while (42)
+	{
+		prompt();
+		get_next_line();
+		get_command();
+		send_to_right_function();
+		if (shell.exit == 1)
+			exit_function();
+	}
 	return (0);
 }
